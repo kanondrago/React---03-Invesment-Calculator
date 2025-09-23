a09dab71fe7bb2713e86584a95f47110586f7e6f
wolfrit <wolfrit.company@gmail.com>
2025-09-24 00:05:12 +0200

    The first commit of the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logoImage from './assets/investment-calculator-logo.png' 
+
+
+function App() {
+  return (
+    <>
+      <body>
+        <div id="header">
+          <img src={logoImage} alt="" />
+          <h1>React Investment Calculator</h1>
+        </div>
+      </body>
+    </>
+  )
+}
+
+export default App
